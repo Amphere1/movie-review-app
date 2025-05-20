cff6ac57af8254457cbe1294717069db9ad3f3eb
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-05-20 21:29:25 +0530

    feat: setted up User model with MongoDB

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { use } from "react";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: [true, "username is required"],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        required: [true, "email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    password:{
+        type: String,
+        required: [true, "password is required"],
+        minLength: [6, "password must be atleast 6 characters long"]
+    },
+    CreatedAt:{
+        type: Date,
+        default: Date.now
+    },
+    UpdatedAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+    next(); 
+});
+
+userSchema.methods.comparePassword = async (password) => {
+    const isMatch = await bcrypt.compare(password, this.password);
+    return isMatch;
+};
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
